fefc915b317ae885b90b28d486fee3795a7495a7
Pablo-RLV <pablo.viana@sou.inteli.edu.br>
2022-06-22 14:55:02 -0300

    Ajax 100% funcional

--- Public/curriculosemana6.js
+++ Public/curriculosemana6.js
@@ -1,19 +1,8 @@
 function escrevertexto() {
     var texto = document.getElementById('comentario').value;
-    var url = "https://jsonplaceholder.typicode.com/todos/1";
-
-    var xhttp = new XMLHttpRequest();
-    xhttp.open("GET", url, false);
-    xhttp.send();//A execução do script pára aqui até a requisição retornar do servidor
-
-    $(getResDiv).append("<br />" + xhttp.responseText);
-    $(getResDiv).append("<br />Seleção do <i>title</i>: " + JSON.parse(xhttp.response).title);
-    //console.log(xhttp.responseText);
     $(".experiencias").append("<li>" + texto + "</li>")
 
 }
-
-  
 var darkmode = 0
 function botao(){
     if(darkmode==1){
@@ -46,3 +35,86 @@ function botao(){
         return darkmode
     }
 }
+
+const rodape = document.getElementById("rodape")
+rodape.onload = geraTabela()
+
+// Requisição por JQuery
+// function geraTabela(){
+// $.getScript("/acessos", function(rows){
+//     console.log(rows + "esse é o Jquery")
+//     let MyJson = JSON.parse(rows)
+//     let MyJsonSize = MyJson.length
+//     for(let i = 0; i < MyJsonSize; i++ ){
+//         document.getElementById("tabela").innerHTML += '<tr>  <td>'+ MyJson[i].id +'</td><td>'+ MyJson[i].name +'</td><td>'+ MyJson[i].date +'</td></tr>'
+//     }
+// });
+// }
+
+
+// Requisição por Ajax
+function geraTabela(){
+    //Objeto request
+    let request = new XMLHttpRequest(); 
+    //Criar a função do pedido
+    request.onreadystatechange = function(){
+        let MyJson = JSON.parse(this.responseText)
+        let MyJsonSize = MyJson.length
+        document.getElementById("tabela").innerHTML = `<tr><td>id</td><td>Nome</td><td>Data</td></tr>`
+        for(let i = 0; i < MyJsonSize; i++ ){
+            document.getElementById("tabela").innerHTML += '<tr id="' + MyJson[i].id + '"> <td>' + MyJson[i].id +'</td><td>'+ MyJson[i].name +'</td><td>'+ MyJson[i].date +'</td><td>' + '<a href="#" class="btndelete" > Remover </a>'  + '</td> </tr>'
+        }
+        console.log(JSON.parse(this.responseText))
+    }
+
+    //Faz o pedido
+    url = "/users"
+    request.open("GET", url, true);
+    request.send();
+}
+
+// Post por ajax
+function enviaAcesso(){
+    const nameInput = document.getElementById("input1").value
+    const dateInput = Number(document.getElementById("input2").value)
+    url = "/adicionaracesso"
+    $.ajax({
+        type: "POST",
+        url: url,
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        data: JSON.stringify(
+            {
+                "name": nameInput,
+                "date": dateInput
+            }
+        )
+    });
+}
+
+// Delete por ajax
+$(document).ready(function(){
+    $('body').on('click', '.btndelete', function (e) {
+        idTable = $(this).closest('tr').attr('id')
+      $.ajax({
+        url: "/deletaracesso",
+        data: { id: idTable},
+        //cache: false,
+        //contentType: false,
+        //processData: false,
+        //mimeType: "multipart/form-data",
+        type: "Delete",
+        dataType: "Json",
+        // success: function(result) {
+        //   if (result.Success) {
+        //     $("#tr39").remove();
+        //   }
+        //   eval(result.Script);
+        // },
+        // error: function() {
+        //   alert("Deu errado meu");
+        // }
+      });
+      $("#"+idTable).remove();
+    });
+  });
--- call.js
+++ call.js
@@ -1,7 +1,7 @@
 const express = require ("express");
 
 //const hostname = "0.0.0.0";
-const port = process.env.PORT || 3000;  
+const port = process.env.PORT || 3500;  
 
 const path = require("path")
 const app = express(); 
@@ -22,7 +22,7 @@ app.get('/users', (req, res) => {
 	res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
 
 	var db = new sqlite3.Database(DBPATH); // Abre o banco
-  var sql = 'SELECT * FROM Habilidades ORDER BY Num ASC';
+  var sql = 'SELECT * FROM curriculo ORDER BY id ASC';
 	db.all(sql, [],  (err, rows ) => {
 		if (err) {
 		    throw err;
@@ -31,6 +31,129 @@ app.get('/users', (req, res) => {
 	});
 	db.close(); // Fecha o banco
 });
+
+app.post('/userinsert', urlencodedParser, (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
+
+	sql = "INSERT INTO Habilidades (Num, Nome) VALUES ('" + req.body.title + "', estudar 12 horas seguidas)";
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+	db.run(sql, [],  err => {
+		if (err) {
+		    throw err;
+		}
+	});
+	db.close(); // Fecha o banco
+	res.end();
+});
+
+// Atualiza um registro (é o U do CRUD - Update)
+app.patch('/userupdate', urlencodedParser, (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
+
+	sql = "UPDATE Habilidades SET title = '" + req.body.title + "' WHERE Num = " + req.body.userId;
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+	db.run(sql, [],  err => {
+		if (err) {
+		    throw err;
+		}
+		res.end();
+	});
+	db.close(); // Fecha o banco
+});
+
+// Exclui um registro (é o D do CRUD - Delete)
+app.delete('/userdelete', urlencodedParser, (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
+
+	sql = "DELETE FROM Habilidades WHERE Num = " + req.body.userId;
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+	db.run(sql, [],  err => {
+		if (err) {
+		    throw err;
+		}
+		res.end();
+	});
+	db.close(); // Fecha o banco
+});
+
+
+
+app.get("/", (req, res) => { 
+    res.sendFile(__dirname + "/public/curriculosemana6.html");
+});
+
+
+
+app.get('/acessos', (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
+
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+  var sql = 'SELECT * FROM curriculo ORDER BY id COLLATE NOCASE';
+	db.all(sql, [],  (err, rows ) => {
+		if (err) {
+		    throw err;
+		
+		}
+	res.json(rows)
+	});
+	db.close(); // Fecha o banco
+});
+
+// Insere um registro (é o C do CRUD - Create)
+app.post('/adicionaracesso', urlencodedParser, (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
+
+	var sql = "INSERT INTO curriculo (name, date) VALUES ('" + req.body.name + "', " + req.body.date + ")";
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+	console.log(sql);
+	db.run(sql, [],  err => {
+		if (err) {
+		    throw err;
+		}
+		else console.log(sql);
+	});
+	db.close(); // Fecha o banco
+	res.end();
+});
+
+// Atualiza um registro (é o U do CRUD - Update)
+app.patch('/atualizaracesso', urlencodedParser, (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
+
+	sql = `UPDATE curriculo SET name = '${req.body.name}', date = ${req.body.date} WHERE id = ${req.body.id} `
+	// "UPDATE curriculo SET name = '" + req.body.name + "' date =  WHERE id = " + req.body.id;
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+	db.run(sql, [],  err => {
+		if (err) {
+		    throw err;
+		}
+		res.end();
+	});
+	db.close(); // Fecha o banco
+});
+
+// Exclui um registro (é o D do CRUD - Delete)
+app.delete('/deletaracesso', urlencodedParser, (req, res) => {
+	res.statusCode = 200;
+	res.setHeader('Access-Control-Allow-Origin', '*'); // Isso é importante para evitar o erro de CORS
+
+	sql = "DELETE FROM curriculo WHERE id = " + req.body.id;
+	var db = new sqlite3.Database(DBPATH); // Abre o banco
+	db.run(sql, [],  err => {
+		if (err) {
+		    throw err;
+		}
+		res.end();
+	});
+	db.close(); // Fecha o banco
+});
+
 
 
 // app.get("/", (req, res) => { 
